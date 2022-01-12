01c8b889bb795060adf6e3a764a96bf3ab037311
Manishankar Vakta <evilrider555@gmail.com>
2022-01-13 02:39:58 +0600

    home page ui

---
+++ lib/page/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final tabs = [
+      Container(
+        child: Center(child: Text("Todos")),
+      ),
+      Container(
+        child: Center(child: Text("Complete")),
+      ),
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(MyApp.title),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white.withOpacity(0.7),
+        selectedItemColor: Colors.white,
+        currentIndex: selectedIndex,
+        onTap: (index) => setState(() {
+          selectedIndex = index;
+        }),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fact_check_outlined),
+            label: "Todos",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.done, size: 28),
+            label: "Completed",
+          ),
+        ],
+      ),
+      body: tabs[selectedIndex],
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Icon(Icons.add),
+        onPressed: () => {},
+      ),
+    );
+  }
+}
